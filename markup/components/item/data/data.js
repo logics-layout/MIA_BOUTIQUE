3c77fa4baedfb38caac599f6d53ad030f57e92ac
logics-layout <git@logics.su>
2017-04-26 17:18:50 +0800

    Фичи

--- markup/components/item/data/data.js
+++ markup/components/item/data/data.js
@@ -16,6 +16,21 @@ var data = {
                     lineThrough: "12 000 р.",
                     default: "8 400 р."
                 }
+            },
+            {
+                stickers: [
+                    {
+                        title: '-30 %',
+                        style: {'background-color': "#ff7132"}
+                    }
+                ],
+                size: ['M', 'S', 'XS'],
+                img: ['assets/item/1', 'assets/item/1', 'assets/item/1'],
+                title: 'Twin-set jeans  /  A62EN',
+                titleSub: 'Платье',
+                price: {
+                    default: "8 400 р."
+                }
             }
         ]
     }
